104e5b070ddf77cde9a8b9ee6310faa297198524
MohitSalvi2207 <mohitsalvi345@gamil.com>
2025-06-26 19:24:30 +0530

    update

--- script.js
+++ script.js
@@ -88,3 +88,12 @@ backToTopBtn.addEventListener("click", (e) => {
 //     icon.classList.add("fa-moon");
 //   }
 // });
+
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("mainNavbar");
+  if (window.scrollY > 10) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+});
